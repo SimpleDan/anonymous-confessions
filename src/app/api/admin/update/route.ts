118f64ab1b53e75ba9f21f5fbc797a19b1b8f309
SimpleDan <31817373+SimpleDan@users.noreply.github.com>
2026-04-13 20:21:26 +0100

    Update route.ts

--- src/app/api/admin/update/route.ts
+++ src/app/api/admin/update/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { supabaseServer } from "src/lib/supabase-server";
+import { supabaseServer } from "../../../../lib/supabase-server";
 
 export async function POST(request: NextRequest) {
   const { password, id, status } = await request.json();
